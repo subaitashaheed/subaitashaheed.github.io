391909e2b87079750b4b3ea44f8bbf842458723a
subaitashaheed <subaitashaheed@gmail.com>
2026-02-17 21:30:59 -0500

    Add files via upload

---
+++ basic-javascript/script.js
@@ -0,0 +1,113 @@
+
+let counter = 0;
+
+
+
+function tickUp() {
+    counter++;
+    document.getElementById("counter").textContent = counter;
+}
+
+function tickDown() {
+    counter--;
+    document.getElementById("counter").textContent = counter;
+}
+
+
+
+
+function runForLoop() {
+    let result = "";
+
+    for (let i = 0; i <= counter; i++) {
+        result += i + " ";
+    }
+
+    document.getElementById("forLoopResult").textContent = result;
+}
+
+
+
+
+function showOddNumbers() {
+    let result = "";
+
+    for (let i = 1; i <= counter; i++) {
+        if (i % 2 !== 0) {
+            result += i + " ";
+        }
+    }
+
+    document.getElementById("oddNumberResult").textContent = result;
+}
+
+
+
+function addMultiplesToArray() {
+    let multiples = [];
+
+    if (counter < 5) {
+        console.log(multiples);
+        return;
+    }
+
+    for (let i = counter; i >= 5; i--) {
+        if (i % 5 === 0) {
+            multiples.push(i);
+        }
+    }
+
+    console.log(multiples);
+}
+
+
+
+
+function printCarObject() {
+    let type = document.getElementById("carType").value;
+    let mpg = document.getElementById("carMPG").value;
+    let color = document.getElementById("carColor").value;
+
+    let car = {
+        cType: type,
+        cMPG: mpg,
+        cColor: color
+    };
+
+    console.log(car);
+}
+
+
+
+
+function loadCar(num) {
+
+    let selectedCar;
+
+    if (num === 1) {
+        selectedCar = carObject1;
+    } else if (num === 2) {
+        selectedCar = carObject2;
+    } else if (num === 3) {
+        selectedCar = carObject3;
+    }
+
+    document.getElementById("carType").value = selectedCar.cType;
+    document.getElementById("carMPG").value = selectedCar.cMPG;
+    document.getElementById("carColor").value = selectedCar.cColor;
+}
+
+
+
+
+function changeColor(num) {
+    let paragraph = document.getElementById("styleParagraph");
+
+    if (num === 1) {
+        paragraph.style.color = "red";
+    } else if (num === 2) {
+        paragraph.style.color = "green";
+    } else if (num === 3) {
+        paragraph.style.color = "blue";
+    }
+}
